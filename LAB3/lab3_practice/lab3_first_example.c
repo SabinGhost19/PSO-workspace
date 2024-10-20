c53f957d8b8376cb6c254f2838b655c557a7c2f9
Sabin <sabinstan19@gmail.com>
2024-10-20 23:18:12 +0300

    adding fork worksss

---
+++ LAB3/lab3_practice/lab3_first_example.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    int pid;
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    printf("<pid:%d>: Executing at line: %d\n", getpid(), __LINE__);
+    pid = fork();
+    if (pid == 0)
+    {
+        printf("<pid:%d>: (copil, variabila pid = %d) Executing at line: %d\n", getpid(), pid, __LINE__);
+        execlp("/bin/ls", "/bin/ls", ".", NULL);
+    }
+    if (pid > 0)
+        printf("<pid:%d>: (parinte, variabila pid = %d) Executing at line: %d\n", getpid(), pid, __LINE__);
+
+    printf("<pid:%d>: Executing at line: %d\n", getpid(), __LINE__);
+
+    return 0;
+}
