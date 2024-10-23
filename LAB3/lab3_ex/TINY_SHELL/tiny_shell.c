62deeb5c5706c71142195982a1d9696902c2386d
Sabin <sabinstan19@gmail.com>
2024-10-23 18:00:20 +0300

    full pipe implementation...WORKSSSS

--- LAB3/lab3_ex/TINY_SHELL/tiny_shell.c
+++ LAB3/lab3_ex/TINY_SHELL/tiny_shell.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #define BUFFER_SIZE 100
 
@@ -53,6 +54,7 @@ char *parse_command(char *line, char **args)
     char *arg_cmd = strtok(line, " ");
     // in lista de argumente primul argument
     // trebuie sa fie numele comenzii
+
     int i = 0;
     while (arg_cmd != NULL && i < 9)
     {
@@ -63,15 +65,33 @@ char *parse_command(char *line, char **args)
     args[i] = NULL; // final lists of arguments
     return args[0];
 }
-
-void simple_cmd(char *command, char *args[10])
+void replace_env_variables_in_args(char *args[])
 {
+    int i = 0;
+    while (args[i] != NULL)
+    {
 
+        if (args[i][0] == '$')
+        {
+            char *actual_env_variable = getenv(args[i] + 1);
+            if (actual_env_variable)
+            {
+                args[i] = actual_env_variable;
+            }
+        }
+        i++;
+    }
+}
+void execution(char *command, char *args[])
+{
+    int status;
     pid_t pid = fork();
     switch (pid)
     {
     case 0:
         // child
+        replace_env_variables_in_args(args);
+
         if (execvp(command, args) == -1)
         {
             perror("execvp failed");
@@ -83,12 +103,176 @@ void simple_cmd(char *command, char *args[10])
         break;
     default:
         // parent
-        wait(NULL);
+        waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status))
+        {
+            printf("Process exited with status: %d\n", WEXITSTATUS(status));
+        }
+        else if (WIFSIGNALED(status))
+        {
+            printf("Process terminated by signal: %d\n", WTERMSIG(status));
+        }
         break;
     }
 }
+void set_environment_variable(char *line)
+{
+    char *variable = strtok(line, "=");
+    char *value = strtok(NULL, "=");
+    if (variable != NULL && value != NULL)
+    {
+        if (setenv(variable, value, 1) < 0)
+        {
+            perror("set env var failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 
-int main()
+void procs_communicate(char *comm, char *args[], char *comm2, char *args2[])
+{
+
+    int pipe_pid_fd[2];
+    if (pipe(pipe_pid_fd) < 0)
+    {
+        perror("error on creating pipe: ");
+        exit(-1);
+    }
+
+    pid_t pid_from_fork_1 = fork();
+    switch (pid_from_fork_1)
+    {
+    case 0:
+        // child
+        close(pipe_pid_fd[0]);
+        dup2(pipe_pid_fd[1], STDOUT_FILENO);
+        // stdout o sa devina intrarea in pipe
+
+        close(pipe_pid_fd[1]);
+
+        replace_env_variables_in_args(args);
+
+        if (execvp(comm, args) < 0)
+        {
+            perror("error laungh pipe cmmd");
+            exit(EXIT_FAILURE);
+        }
+        break;
+    case -1:
+        perror("Error forking first process");
+        exit(EXIT_FAILURE);
+    default:
+        // parent
+        // nothing
+        // lasam ca procesele sa se execute in paralel
+        // pentru un flux mai bun
+        break;
+    }
+
+    pid_t pid_from_fork_2 = fork();
+    switch (pid_from_fork_2)
+    {
+    case 0:
+        // child
+        close(pipe_pid_fd[1]);
+        dup2(pipe_pid_fd[0], STDIN_FILENO);
+        // stdin o sa devina iesirea din pipe
+        // practic stdin o sa pointeze spre fd de la pipe_pid_fd[0]--iesirea
+        close(pipe_pid_fd[0]);
+        // nu mai e nevoie de acesta asa ca in inchidem
+        // avem deja fd pentru iesire pipe in stdin_fileno
+
+        replace_env_variables_in_args(args);
+
+        if (execvp(comm2, args2) < 0)
+        {
+            perror("error laungh pipe cmmd");
+        }
+        exit(EXIT_FAILURE);
+        break;
+    case -1:
+        perror("Error forking second process");
+        exit(EXIT_FAILURE);
+    default:
+        // parent
+        // nothing
+        // lasam ca procesele sa se execute in paralel
+        // pentru un flux mai bun
+        break;
+    }
+    // inchidem ambele capete pentru ca nu avem nevoie de ele
+    close(pipe_pid_fd[0]);
+    close(pipe_pid_fd[1]);
+
+    // asteptam ca ambele procese sa se termine
+    int status1, status2;
+    waitpid(pid_from_fork_1, &status1, 0);
+    waitpid(pid_from_fork_2, &status2, 0);
+
+    // just for debugging
+    if (WIFEXITED(status1))
+    {
+        printf(" 1 Process exited with status: %d\n", WEXITSTATUS(status1));
+    }
+    else if (WIFSIGNALED(status1))
+    {
+        printf("1 Process terminated by signal: %d\n", WTERMSIG(status1));
+    }
+    if (WIFEXITED(status2))
+    {
+        printf(" 2 Process exited with status: %d\n", WEXITSTATUS(status2));
+    }
+    else if (WIFSIGNALED(status2))
+    {
+        printf(" 2 Process terminated by signal: %d\n", WTERMSIG(status2));
+    }
+}
+char *trim(char *str)
+{
+    // Eliminam spatiile de la inceput
+    while (isspace((unsigned char)*str))
+        str++;
+
+    if (*str == 0) // Verificam daca a ramas vreun caracter
+        return str;
+
+    // Eliminam spatiile de la sfarsit
+    char *end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end))
+        end--;
+
+    // Terminam sirul de caractere
+    *(end + 1) = '\0';
+
+    return str;
+}
+void parse_pipes_commands(char *line)
+{
+    char *commands__[10];
+
+    char *command = strtok(line, "|");
+
+    int i = 0;
+    while (command != NULL)
+    {
+        commands__[i] = trim(command);
+        command = strtok(NULL, "|");
+        i++;
+    }
+    commands__[i] = NULL;
+
+    char *args[10];
+    char *args2[10];
+
+    for (int k = 0; k < i - 1; k++)
+    {
+        char *comm = parse_command(commands__[k], args);
+        char *comm2 = parse_command(commands__[k + 1], args2);
+        procs_communicate(comm, args, comm2, args2);
+    }
+}
+int main(int argc, char *argv[], char *envp[])
 {
     setvbuf(stdout, NULL, _IONBF, 0);
 
@@ -102,6 +286,20 @@ int main()
         // citire linie
         char *line = read_line();
 
+        if (strchr(line, '|') != NULL)
+        {
+            parse_pipes_commands(line);
+            continue;
+        }
+
+        if (strchr(line, '=') != NULL)
+        {
+            set_environment_variable(line); // setam variabila de mediu
+            // o adaugam in varibilele de mediu iar dupa o extragem
+            free(line);
+            continue;
+        }
+
         // extragere first comand
         first_command = parse_command(line, args);
 
@@ -112,7 +310,7 @@ int main()
         }
 
         // exec
-        simple_cmd(first_command, args);
+        execution(first_command, args);
 
         free(line);
     }
