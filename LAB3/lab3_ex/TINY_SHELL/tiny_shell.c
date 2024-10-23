2d6887bd8b60ff4615d576dd671d7b28b257edb8
Sabin <sabinstan19@gmail.com>
2024-10-23 11:07:03 +0300

    tiny_shell--little work

--- LAB3/lab3_ex/TINY_SHELL/tiny_shell.c
+++ LAB3/lab3_ex/TINY_SHELL/tiny_shell.c
@@ -7,22 +7,24 @@
 #include <sys/wait.h>
 
 #define BUFFER_SIZE 100
+
+// Functie pentru extragerea primei comenzi
 char *extract_first_cmd(char *buffer)
 {
     char *cmd = (char *)malloc(sizeof(char) * 100);
     int i = 0;
-    while (buffer[i] != '\n' && buffer[i] != ' ')
+    while (buffer[i] != '\n' && buffer[i] != ' ' && buffer[i] != '\0')
     {
         cmd[i] = buffer[i];
         i++;
     }
     cmd[i] = '\0';
-
     return cmd;
 }
+
+// Functie pentru citirea inputului din linia de comanda
 char *read_line()
 {
-
     char *buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
     printf("> ");
     ssize_t read_bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE);
@@ -33,29 +35,48 @@ char *read_line()
         exit(-1);
     }
 
-    buffer[read_bytes] = '\0';
+    // Eliminam newline-ul '\n' de la final
+    if (read_bytes > 0 && buffer[read_bytes - 1] == '\n')
+    {
+        buffer[read_bytes - 1] = '\0';
+    }
+    else
+    {
+        buffer[read_bytes] = '\0';
+    }
 
     return buffer;
 }
-void simple_cmd(char *command)
+char *parse_command(char *line, char **args)
 {
-    command[strcspn(command, "\n")] = '\0';
-    char *args[10]; // Maxim 10 argumente
+
+    char *arg_cmd = strtok(line, " ");
+    // in lista de argumente primul argument
+    // trebuie sa fie numele comenzii
     int i = 0;
-    // spargem comadna in argumente
-    args[i] = strtok(command, " ");
-    while (args[i] != NULL && i < 9)
+    while (arg_cmd != NULL && i < 9)
     {
+        args[i] = arg_cmd;
         i++;
-        args[i] = strtok(NULL, " ");
+        arg_cmd = strtok(NULL, " ");
     }
+    args[i] = NULL; // final lists of arguments
+    return args[0];
+}
+
+void simple_cmd(char *command, char *args[10])
+{
 
     pid_t pid = fork();
     switch (pid)
     {
     case 0:
         // child
-        int status = execvp(args[0], args);
+        if (execvp(command, args) == -1)
+        {
+            perror("execvp failed");
+        }
+        exit(EXIT_FAILURE);
         break;
     case -1:
         perror("Eroare la fork");
@@ -66,16 +87,35 @@ void simple_cmd(char *command)
         break;
     }
 }
+
 int main()
 {
-
     setvbuf(stdout, NULL, _IONBF, 0);
-    char first_cmd[100];
-    while (strcmp(first_cmd, "exit") != 0)
+
+    char *first_command;
+    char *args[10]; // Argumentele comenzii
+    // maxim 10 argumente
+
+    while (1)
     {
+
+        // citire linie
         char *line = read_line();
-        strcpy(first_cmd, extract_first_cmd(line));
-        simple_cmd(first_cmd);
+
+        // extragere first comand
+        first_command = parse_command(line, args);
+
+        if (strcmp(first_command, "exit") == 0)
+        {
+            free(line);
+            break;
+        }
+
+        // exec
+        simple_cmd(first_command, args);
+
+        free(line);
     }
+
     return 0;
 }
